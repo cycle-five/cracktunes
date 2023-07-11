32b50e5b020a5de9151d2dc381d4add79640b9c4
Cycle Five <cycle.five@proton.me>
2023-07-10 22:55:40 -0400

    get rid of arc in shuttle stuff

--- src/lib.rs
+++ src/lib.rs
@@ -178,10 +178,10 @@ impl Default for Data {
 use std::net::SocketAddr;
 
 /// A wrapper type for [poise::Framework] so we can implement [shuttle_runtime::Service] for it.
-pub struct PoiseService<T, E>(pub Arc<poise::Framework<Arc<T>, E>>);
+pub struct PoiseService<T, E>(pub Arc<poise::Framework<T, E>>);
 
 #[shuttle_runtime::async_trait]
-impl<T, E> shuttle_runtime::Service for PoiseService<Arc<T>, E>
+impl<T, E> shuttle_runtime::Service for PoiseService<T, E>
 where
     T: Send + Sync + 'static,
     E: Send + Sync + 'static,
@@ -196,8 +196,8 @@ where
     }
 }
 
-impl<T, E> From<Arc<poise::Framework<Arc<T>, E>>> for PoiseService<T, E> {
-    fn from(framework: Arc<poise::Framework<Arc<T>, E>>) -> Self {
+impl<T, E> From<Arc<poise::Framework<T, E>>> for PoiseService<T, E> {
+    fn from(framework: Arc<poise::Framework<T, E>>) -> Self {
         Self(framework)
     }
 }
