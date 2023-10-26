9f21078bec8a0b16116dd8f8de0aee5cfceecc88
Cycle Five <cycle.five@proton.me>
2023-10-25 23:07:19 -0400

    vagrants

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-if $# -eq 0; then
+if [ $# -eq 0 ]; then
 	echo "Usage: $0 [all|server(s)]"
 	exit 1
 fi
 
-if [ $1 = "all" ]; then
+if [ "$1" = "all" ]; then
 	SERVERS=("kalevala" "hamr" "poseidon")
 else
 	SERVERS=("$*")
 fi
 
-SERVERS=("kalevala" "hamr")
+# SERVERS=("kalevala" "hamr")
 for server in "${SERVERS[@]}"; do
 	rsync -urltv --exclude .env --exclude 'target' --exclude 'data/settings' --exclude cracktunes.toml -e ssh /home/lothrop/dev/cracktunes "${server}:~/src/"
 done
