4ee1e84fbebd1b8f63f9de2fec5f159e9a10aaa8
Cycle Five <cycle.five@proton.me>
2023-11-06 00:18:49 -0500

    delete channel

--- crack-core/src/commands/admin.rs
+++ crack-core/src/commands/admin.rs
@@ -43,6 +43,7 @@ use crate::{
         "undeafen",
         "create_voice_channel",
         "create_text_channel",
+        "delete_channel",
         "create_role",
         "audit_logs",
     ),
@@ -849,48 +850,49 @@ pub async fn create_role(ctx: Context<'_>, role_name: String) -> Result<(), Erro
     Ok(())
 }
 
-// /// Delete channel.
-// #[poise::command(prefix_command, owners_only, ephemeral)]
-// pub async fn delete_channel(ctx: Context<'_>, channel_name: String) -> Result<(), Error> {
-//     match ctx.guild_id() {
-//         Some(guild) => {
-//             let guild = guild.to_partial_guild(&ctx).await?;
-//             let channel = guild
-//                 .channels(&ctx)
-//                 .await?
-//                 .into_iter()
-//                 .find(|channel_id, guild_channel| channel_id.name == channel_name);
-//             if let Some((channel_id, guild_chan)) = channel {
-//                 if let Err(e) = guild_chan.delete(&ctx).await {
-//                     // Handle error, send error message
-//                     create_response_poise(
-//                         ctx,
-//                         CrackedMessage::Other(format!("Failed to delete channel: {}", e)),
-//                     )
-//                     .await?;
-//                 } else {
-//                     // Send success message
-//                     create_response_poise(
-//                         ctx,
-//                         CrackedMessage::ChannelDeleted {
-//                             channel_name: channel_name.clone(),
-//                         },
-//                     )
-//                     .await?;
-//                 }
-//             } else {
-//                 create_response_poise(ctx, CrackedMessage::Other("Channel not found.".to_string()))
-//                     .await?;
-//             }
-//         }
-//         None => {
-//             return Result::Err(
-//                 CrackedError::Other("This command can only be used in a guild.".to_string()).into(),
-//             );
-//         }
-//     }
-//     Ok(())
-// }
+/// Delete channel.
+#[poise::command(prefix_command, owners_only, ephemeral)]
+pub async fn delete_channel(ctx: Context<'_>, channel_name: String) -> Result<(), Error> {
+    match ctx.guild_id() {
+        Some(guild) => {
+            let guild = guild.to_partial_guild(&ctx).await?;
+            let channel = guild
+                .channels(&ctx)
+                .await?
+                .into_iter()
+                .find(|(_channel_id, guild_chan)| guild_chan.name == channel_name);
+            if let Some((channel_id, guild_chan)) = channel {
+                if let Err(e) = guild_chan.delete(&ctx).await {
+                    // Handle error, send error message
+                    create_response_poise(
+                        ctx,
+                        CrackedMessage::Other(format!("Failed to delete channel: {}", e)),
+                    )
+                    .await?;
+                } else {
+                    // Send success message
+                    create_response_poise(
+                        ctx,
+                        CrackedMessage::ChannelDeleted {
+                            channel_id: channel_id,
+                            channel_name: channel_name.clone(),
+                        },
+                    )
+                    .await?;
+                }
+            } else {
+                create_response_poise(ctx, CrackedMessage::Other("Channel not found.".to_string()))
+                    .await?;
+            }
+        }
+        None => {
+            return Result::Err(
+                CrackedError::Other("This command can only be used in a guild.").into(),
+            );
+        }
+    }
+    Ok(())
+}
 
 // /// Delete role.
 // #[poise::command(prefix_command, , owners_only, ephemeral)]
