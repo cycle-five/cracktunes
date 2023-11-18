2c3f9eee7b6cc7ebd5b2c9f6fc8dd70df4961ba6
Cycle Six <cycle.six@skiff.com>
2023-11-18 09:06:24 +0100

    Simplify summon

--- crack-core/src/commands/music/summon.rs
+++ crack-core/src/commands/music/summon.rs
@@ -2,6 +2,7 @@ use std::sync::Arc;
 use std::time::Duration;
 
 use self::serenity::{model::id::ChannelId, Mentionable};
+use crate::handlers::voice::register_voice_handlers;
 use crate::handlers::IdleHandler;
 use crate::{
     connection::get_voice_channel_for_user,
@@ -15,9 +16,9 @@ use crate::{
     Context,
     Error,
 };
-use ::serenity::all::{Channel, Guild, UserId};
+use ::serenity::all::{Channel, Guild, GuildId, UserId};
 use poise::{serenity_prelude as serenity, CreateReply};
-use songbird::Call;
+use songbird::{Call, Songbird};
 use songbird::{Event, TrackEvent};
 use tokio::sync::Mutex;
 // use std::{sync::Arc, time::Duration};
@@ -67,8 +68,8 @@ pub async fn summon(
         }),
     }?;
 
-    // // join the channel
-    // let result = manager.join(guild.id, channel_id).await?;
+    register_track_end_handler(ctx, guild_id, channel_id, call.clone(), manager.clone()).await?;
+
     let buffer = {
         // // Open the data lock in write mode, so keys can be inserted to it.
         // let mut data = ctx.data().write().await;
@@ -79,53 +80,56 @@ pub async fn summon(
         data.clone()
     };
 
-    use crate::handlers::voice::register_voice_handlers;
-
-    // FIXME
-    // use crate::handlers::voice::register_voice_handlers;
-
     let _ = register_voice_handlers(buffer, call.clone()).await;
-    {
-        let mut handler = call.lock().await;
-        // unregister existing events and register idle notifier
-        handler.remove_all_global_events();
 
-        let guild_settings_map = ctx.data().guild_settings_map.read().unwrap().clone();
-
-        let _ = guild_settings_map.get(&guild_id).map(|guild_settings| {
-            let timeout = guild_settings.timeout;
-            if timeout > 0 {
-                handler.add_global_event(
-                    Event::Periodic(Duration::from_secs(1), None),
-                    IdleHandler {
-                        http: ctx.serenity_context().http.clone(),
-                        manager: manager.clone(),
-                        channel_id,
-                        guild_id: Some(guild_id),
-                        limit: timeout as usize,
-                        count: Default::default(),
-                    },
-                );
-            }
-        });
-
-        handler.add_global_event(
-            Event::Track(TrackEvent::End),
-            TrackEndHandler {
-                guild_id: guild.id,
-                http: ctx.serenity_context().http.clone(),
-                call: call.clone(),
-                data: ctx.data().clone(),
-            },
-        );
-
-        let text = CrackedMessage::Summon {
-            mention: channel_id.mention(),
-        }
-        .to_string();
-        ctx.send(CreateReply::new().content(text).ephemeral(true))
-            .await?;
+    let text = CrackedMessage::Summon {
+        mention: channel_id.mention(),
     }
+    .to_string();
+    ctx.send(CreateReply::new().content(text).ephemeral(true))
+        .await?;
+
+    Ok(())
+}
+
+async fn register_track_end_handler(
+    ctx: Context<'_>,
+    guild_id: GuildId,
+    channel_id: ChannelId,
+    call: Arc<Mutex<Call>>,
+    manager: Arc<Songbird>,
+) -> Result<(), Error> {
+    let mut handler = call.lock().await;
+    // unregister existing events and register idle notifier
+    handler.remove_all_global_events();
+
+    let guild_settings_map = ctx.data().guild_settings_map.read().unwrap().clone();
+
+    let _ = guild_settings_map.get(&guild_id).map(|guild_settings| {
+        let timeout = guild_settings.timeout;
+        if timeout > 0 {
+            handler.add_global_event(
+                Event::Periodic(Duration::from_secs(60), None),
+                IdleHandler {
+                    http: ctx.serenity_context().http.clone(),
+                    manager: manager.clone(),
+                    channel_id,
+                    guild_id: Some(guild_id),
+                    limit: timeout as usize,
+                    count: Default::default(),
+                },
+            );
+        }
+    });
+    handler.add_global_event(
+        Event::Track(TrackEvent::End),
+        TrackEndHandler {
+            guild_id,
+            http: ctx.serenity_context().http.clone(),
+            call: call.clone(),
+            data: ctx.data().clone(),
+        },
+    );
 
     Ok(())
 }
@@ -140,19 +144,12 @@ async fn get_channel_id_for_summon(
         return Ok(channel.id());
     }
 
-    match channel_id_str {
-        Some(id) => {
-            tracing::warn!("channel_id_str: {:?}", id);
-            match id.parse::<u64>() {
-                Ok(id) => Ok(ChannelId::new(id)),
-                Err(_) => match get_voice_channel_for_user(&guild, &user_id) {
-                    Some(channel_id) => Ok(channel_id),
-                    None => get_voice_channel_for_user_with_error(&guild, &user_id),
-                },
-            }
+    if let Some(channel_id_str) = channel_id_str {
+        if let Ok(id) = channel_id_str.parse::<u64>() {
+            return Ok(ChannelId::new(id));
         }
-        None => get_voice_channel_for_user_with_error(&guild, &user_id),
     }
+    get_voice_channel_for_user_with_error(&guild, &user_id)
 }
 
 fn get_voice_channel_for_user_with_error(
