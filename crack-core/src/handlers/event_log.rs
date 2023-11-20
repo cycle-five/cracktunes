f6b09e8a1be2877377717a55a38b80f30b8c112b
Cycle Six <cycle.six@skiff.com>
2023-11-20 02:10:43 +0100

    cleanup event_log

--- crack-core/src/handlers/event_log.rs
+++ crack-core/src/handlers/event_log.rs
@@ -285,7 +285,11 @@ pub async fn handle_event(
             event_name
         ),
         FullEvent::ChannelUpdate { ctx, old, new } => {
-            let guild_id = new.clone().guild().map(|x| x.guild_id).unwrap_or_default();
+            let guild_id = new
+                .clone()
+                .guild(&ctx.cache)
+                .map(|x| x.id)
+                .unwrap_or_default();
             log_event!(
                 log_unimplemented_event,
                 guild_settings,
