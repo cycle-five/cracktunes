e0e1447cf044643383bcef17213c0a1673b86572
Cycle Five <cycle.five@proton.me>
2023-12-08 22:40:28 -0500

    fix move users?

--- crack-core/src/commands/admin/move_users.rs
+++ crack-core/src/commands/admin/move_users.rs
@@ -2,25 +2,31 @@ use crate::errors::CrackedError;
 use crate::Context;
 use crate::Error;
 use serenity::all::ChannelType;
-use serenity::all::GuildChannel;
-use serenity::all::Member;
+use serenity::all::{ChannelId, UserId};
 use serenity::builder::EditMember;
 
 /// Move usrers to a given channel.
 #[poise::command(rename = "move_users_to", prefix_command, owners_only, ephemeral)]
 pub async fn move_users_to(
     ctx: Context<'_>,
-    #[description = "Users to move"] users: Vec<Member>,
-    #[description = "Channel to move users to"] guild_chan_to: GuildChannel,
+    #[description = "Users to move"] user_ids: Vec<UserId>,
+    #[description = "Channel to move users to"] chan_id: ChannelId,
 ) -> Result<(), Error> {
     // Check if the Channel's are voice channels
+    let guild_id = ctx.guild_id().ok_or(CrackedError::NoGuildId)?;
+    let channels = guild_id.channels(ctx).await?;
+    let guild_chan_to = channels
+        .get(&chan_id)
+        .ok_or(CrackedError::Other("Channel not found"))?;
     if guild_chan_to.kind != ChannelType::Voice {
         return Result::Err(
             CrackedError::Other("This command can only be used in a guild.").into(),
         );
     }
 
-    for member in users.clone().iter_mut() {
+    for user_id in user_ids.iter() {
+        let mut member = ctx.http().get_member(guild_id, *user_id).await?;
+
         let _ = member
             .edit(ctx, EditMember::new().voice_channel(guild_chan_to.id))
             .await;
