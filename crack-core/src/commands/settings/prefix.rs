a4ac82250be1a74027aade97c95abaa74c197993
Cycle Five <cycle.five@proton.me>
2023-12-13 17:55:17 -0500

    add clear prefix and reorganize

--- crack-core/src/commands/settings/prefix.rs
+++ crack-core/src/commands/settings/prefix.rs
@@ -5,7 +5,7 @@ use crate::utils::send_response_poise;
 use crate::Context;
 use crate::Error;
 
-/// Set the prefix for the bot.
+/// Add an additional prefic to the bot.
 #[poise::command(prefix_command, owners_only)]
 pub async fn add_prefix(
     ctx: Context<'_>,
@@ -24,6 +24,38 @@ pub async fn add_prefix(
                     .into_iter()
                     .chain(vec![prefix.clone()])
                     .collect();
+            })
+            .or_insert(GuildSettings::new(
+                guild_id,
+                Some(&prefix.clone()),
+                Some(guild_name),
+            ));
+        new_settings.additional_prefixes.clone()
+    };
+    send_response_poise(
+        ctx,
+        CrackedMessage::Other(format!(
+            "Current additional prefixes {}",
+            additional_prefixes.join(", ")
+        )),
+    )
+    .await?;
+    Ok(())
+}
+/// Add an additional prefic to the bot.
+#[poise::command(prefix_command, owners_only)]
+pub async fn clear_prefixes(
+    ctx: Context<'_>,
+    #[description = "The prefix to add to the bot"] prefix: String,
+) -> Result<(), Error> {
+    let guild_id = ctx.guild_id().unwrap();
+    let guild_name = get_guild_name(ctx.serenity_context(), guild_id).unwrap_or_default();
+    let additional_prefixes = {
+        let mut settings = ctx.data().guild_settings_map.write().unwrap();
+        let new_settings = settings
+            .entry(guild_id)
+            .and_modify(|e| {
+                e.additional_prefixes = vec![];
             })
             .or_insert(GuildSettings::new(
                 guild_id,
--- crack-core/src/commands/settings/set_all_log_channel.rs
+++ crack-core/src/commands/settings/set_all_log_channel.rs
@@ -2,9 +2,30 @@ use crate::{
     commands::set_all_log_channel_data, messaging::message::CrackedMessage,
     utils::send_response_poise, Context, Error,
 };
-use serenity::all::Channel;
+use serenity::all::{Channel, GuildId};
+use serenity::model::id::ChannelId;
 
-/// Set the join-leave log channel.
+/// Set a log channel for a specific guild.
+#[poise::command(prefix_command, owners_only)]
+pub async fn set_log_channel_for_guild(
+    ctx: Context<'_>,
+    #[description = "GuildId to set logging for"] guild_id: GuildId,
+    #[description = "ChannelId to sendlogs"] channel_id: ChannelId,
+    #[description = "Type of logs to send"] _log_type: String,
+) -> Result<(), Error> {
+    // set_all_log_channel_old_data(ctx.serenity_context().data.clone(), guild_id, channel_id).await?;
+    set_all_log_channel_data(ctx.data(), guild_id, channel_id).await?;
+
+    send_response_poise(
+        ctx,
+        CrackedMessage::Other(format!("all log channel set to {}", channel_id)),
+    )
+    .await?;
+
+    Ok(())
+}
+
+/// Set a channel to send all logs.
 #[poise::command(prefix_command, owners_only)]
 pub async fn set_all_log_channel(
     ctx: Context<'_>,
