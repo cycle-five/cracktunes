13c2185dc96a675b1d00e7e0a50f52aeb3aae203
Cycle Five <cycle.five@proton.me>
2023-12-14 02:58:44 -0500

    playlog

--- crack-core/src/db/play_log.rs
+++ crack-core/src/db/play_log.rs
@@ -1,9 +1,33 @@
-use sqlx::types::chrono::{self};
+use sqlx::types::chrono::NaiveDateTime;
 
 pub struct PlayLog {
     pub id: i64,
     pub user_id: i64,
     pub guild_id: i64,
     pub metadata_id: i64,
-    pub created_at: chrono::DateTime<chrono::Utc>,
+    pub created_at: NaiveDateTime,
+}
+
+impl PlayLog {
+    pub async fn create(
+        conn: &mut sqlx::PgConnection,
+        user_id: i64,
+        guild_id: i64,
+        metadata_id: i64,
+    ) -> Result<Self, sqlx::Error> {
+        let play_log = sqlx::query_as!(
+            PlayLog,
+            r#"
+            INSERT INTO play_log (user_id, guild_id, metadata_id)
+            VALUES ($1, $2, $3)
+            RETURNING id, user_id, guild_id, metadata_id, created_at
+            "#,
+            user_id,
+            guild_id,
+            metadata_id
+        )
+        .fetch_one(conn)
+        .await?;
+        Ok(play_log)
+    }
 }
--- migrations/20231204072029_play_log.sql
+++ migrations/20231204072029_play_log.sql
@@ -1,9 +1,9 @@
 -- Add migration script here
 CREATE TABLE IF NOT EXISTS play_log (
     id SERIAL PRIMARY KEY,
-    user_id INT NOT NULL,
+    user_id BIGINT NOT NULL,
     guild_id BIGINT NOT NULL,
-    metadata_id INT NOT NULL,
+    metadata_id BIGINT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT play_log_fk_constraint FOREIGN KEY (user_id) REFERENCES "user"(id),
     FOREIGN KEY (metadata_id) REFERENCES metadata(id),
