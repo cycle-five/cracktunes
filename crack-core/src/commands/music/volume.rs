d4c82d66937610990a3f1d5454f2c76183b96919
Cycle Five <cycle.five@proton.me>
2023-12-21 11:46:10 -0500

    fix setting volume (again)

--- crack-core/src/commands/music/volume.rs
+++ crack-core/src/commands/music/volume.rs
@@ -103,8 +103,8 @@ pub async fn volume(
 
         let new_vol = to_set.unwrap() as f32 / 100.0;
         let old_vol = {
-            let mut asdf = ctx.data().guild_settings_map.write().unwrap().clone();
-            let guild_settings = asdf
+            let mut guild_settings_guard = ctx.data().guild_settings_map.write().unwrap();
+            let guild_settings = guild_settings_guard
                 .entry(guild_id)
                 .and_modify(|guild_settings| {
                     guild_settings.set_volume(new_vol);
@@ -126,16 +126,11 @@ pub async fn volume(
                     guild_settings
                 });
             tracing::warn!(
-                "guild_settings: {:?}",
+                "guild_settings: {}",
                 format!("{:?}", guild_settings).white(),
             );
             let old_volume = guild_settings.old_volume;
-            match guild_settings.save().await {
-                Ok(_) => (),
-                Err(e) => {
-                    tracing::error!("Error saving guild_settings: {:?}", e);
-                }
-            }
+
             old_volume
         };
 
