ac934ea9890cdd366d03a8fcee305be664f44c13
Replit user <>
2023-12-21 04:59:50 +0000

    resetdb script and cleanup replit configs

--- scripts/reset_db.sh
+++ scripts/reset_db.sh
@@ -1,7 +1,16 @@
-#!/bin/bash
-export DATABASE_URL=postgresql://postgres:mysecretpassword@localhost:5433/postgres
+#!/bin/sh
+set -ex
+
+if [ "${1}" != "replit" ]; then
+	export DATABASE_URL=postgresql://postgres:mysecretpassword@localhost:5433/postgres
+fi
 # cargo clean
 sqlx database drop
 sqlx database create
 sqlx migrate run --source migrations/
-cargo sqlx prepare --workspace
+
+if [ "${1}" = "replit" ]; then
+	cargo sqlx prepare --merged
+else
+	cargo sqlx prepare --workspace
+fi
