ee93c6c3847d555cc9184c222cf2502151f4f4d2
Cycle Five <cycle.five@proton.me>
2023-12-25 22:36:04 +0000

    add psql client install script

---
+++ scripts/install_psql.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -ex
+
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg
+sudo apt update
+sudo apt install -y postgresql-16 postgresql-contrib-16
