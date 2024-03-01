74945b113187583ece1d40a4ef57ecf9fe287cd8
Cycle Five <cycle.five@proton.me>
2024-03-01 09:27:08 -0500

    add feature for osint

---
+++ crack-core/src/commands/osint/mod.rs
@@ -0,0 +1,3 @@
+pub mod scan;
+
+pub use scan::*;
---
+++ crack-core/src/commands/osint/scan.rs
@@ -0,0 +1,23 @@
+use crack_osint::scan;
+
+/// Scan a website for viruses or malicious content.
+///
+/// Other scanning options include VirusTotal, Google Safe Browsing, Metadefender, etc.
+///
+/// # Arguments
+/// * `url` - The URL of the website to be scanned.
+/// curl --request POST \
+///     --url https://www.virustotal.com/api/v3/urls \
+///     --form url=<Your URL here> \
+///     --header 'x-apikey: <your API key>'
+#[poise::command(prefix_command, hide_in_help)]
+pub async fn scan(ctx: Context<'_>, url: String) -> Result<(), Error> {
+    let guild_id = ctx.guild_id().unwrap_or()?;
+
+    let message = scan(ctx, url).await;
+
+    // Send the response to the user
+    send_response_poise(ctx, CrackedMessage::ScanResult { result: message })
+        .await
+        .map(|m| ctx.data().add_msg_to_cache(ctx.guild_id().unwrap(), m))
+}
