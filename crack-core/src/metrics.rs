1e11160fc4eef01caaf4eeb21565feab9b3de92c
Cycle Five <cycle.five@proton.me>
2024-03-16 05:32:45 +0000

    Mar 16, 2024, 12:32 AM

--- crack-core/src/metrics.rs
+++ crack-core/src/metrics.rs
@@ -1,62 +1,61 @@
-use lazy_static::lazy_static;
 #[cfg(feature = "crack-metrics")]
-use prometheus::{Gauge, HistogramOpts, HistogramVec, IntCounterVec, Opts, Registry};
+mod metrics {
+    use lazy_static::lazy_static;
+    use prometheus::{Gauge, HistogramOpts, HistogramVec, IntCounterVec, Opts, Registry};
 
-#[cfg(feature = "crack-metrics")]
-lazy_static! {
-    pub static ref REGISTRY: Registry = Registry::new();
-    pub static ref COMMAND_EXECUTIONS: IntCounterVec = IntCounterVec::new(
-        Opts::new("command_executions", "Command Executions"),
-        &["command", "type"]
-    )
-    .expect("metric can be created");
-    pub static ref COMMAND_ERRORS: IntCounterVec =
-        IntCounterVec::new(Opts::new("command_errors", "Command Errors"), &["command"])
-            .expect("metric can be created");
-    pub static ref CONNECTED_CLIENTS: Gauge =
-        Gauge::new("connected_clients", "Connected Clients").expect("metric can be created");
-    pub static ref RESPONSE_TIME_COLLECTOR: HistogramVec = HistogramVec::new(
-        HistogramOpts::new("response_time", "Response Times"),
-        &["command"]
-    )
-    .expect("metric can be created");
-}
+    lazy_static! {
+        pub static ref REGISTRY: Registry = Registry::new();
+        pub static ref COMMAND_EXECUTIONS: IntCounterVec = IntCounterVec::new(
+            Opts::new("command_executions", "Command Executions"),
+            &["command", "type"]
+        )
+        .expect("metric can be created");
+        pub static ref COMMAND_ERRORS: IntCounterVec =
+            IntCounterVec::new(Opts::new("command_errors", "Command Errors"), &["command"])
+                .expect("metric can be created");
+        pub static ref CONNECTED_CLIENTS: Gauge =
+            Gauge::new("connected_clients", "Connected Clients").expect("metric can be created");
+        pub static ref RESPONSE_TIME_COLLECTOR: HistogramVec = HistogramVec::new(
+            HistogramOpts::new("response_time", "Response Times"),
+            &["command"]
+        )
+        .expect("metric can be created");
+    }
 
-/// Register custom metrics with the prometheus registry
-#[cfg(feature = "crack-metrics")]
-pub fn register_custom_metrics() {
-    REGISTRY
-        .register(Box::new(COMMAND_EXECUTIONS.clone()))
-        .expect("collector can be registered");
+    /// Register custom metrics with the prometheus registry
+    pub fn register_custom_metrics() {
+        REGISTRY
+            .register(Box::new(COMMAND_EXECUTIONS.clone()))
+            .expect("collector can be registered");
 
-    REGISTRY
-        .register(Box::new(CONNECTED_CLIENTS.clone()))
-        .expect("collector can be registered");
+        REGISTRY
+            .register(Box::new(CONNECTED_CLIENTS.clone()))
+            .expect("collector can be registered");
 
-    REGISTRY
-        .register(Box::new(COMMAND_ERRORS.clone()))
-        .expect("collector can be registered");
+        REGISTRY
+            .register(Box::new(COMMAND_ERRORS.clone()))
+            .expect("collector can be registered");
 
-    REGISTRY
-        .register(Box::new(RESPONSE_TIME_COLLECTOR.clone()))
-        .expect("collector can be registered");
-}
+        REGISTRY
+            .register(Box::new(RESPONSE_TIME_COLLECTOR.clone()))
+            .expect("collector can be registered");
+    }
 
-#[cfg(feature = "crack-metrics")]
-#[cfg(test)]
-mod test {
-    use super::*;
+    #[cfg(test)]
+    mod test {
+        use super::*;
 
-    #[test]
-    pub fn metrics_test() {
-        register_custom_metrics();
-        COMMAND_EXECUTIONS
-            .with_label_values(&["test", "test"])
-            .inc();
-        CONNECTED_CLIENTS.inc();
-        COMMAND_ERRORS.with_label_values(&["test"]).inc();
-        RESPONSE_TIME_COLLECTOR
-            .with_label_values(&["test"])
-            .observe(1.0);
+        #[test]
+        pub fn metrics_test() {
+            register_custom_metrics();
+            COMMAND_EXECUTIONS
+                .with_label_values(&["test", "test"])
+                .inc();
+            CONNECTED_CLIENTS.inc();
+            COMMAND_ERRORS.with_label_values(&["test"]).inc();
+            RESPONSE_TIME_COLLECTOR
+                .with_label_values(&["test"])
+                .observe(1.0);
+        }
     }
 }
