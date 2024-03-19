b7defc527cb6ddcf8bfb1625459a665324424ec2
Cycle Five <cycle.five@proton.me>
2024-03-19 01:31:21 +0000

    Mar 18, 2024, 8:31 PM

--- crack-core/src/handlers/serenity.rs
+++ crack-core/src/handlers/serenity.rs
@@ -31,12 +31,12 @@ pub struct SerenityHandler {
     pub is_loop_running: std::sync::atomic::AtomicBool,
 }
 
+/// 
 #[derive(Copy, Clone, Debug)]
 pub struct MyVoiceUserInfo {
     pub user_id: UserId,
     pub guild_id: GuildId,
     pub channel_id: ChannelId,
-    // true = on, false = off
     pub camera_status: bool,
     pub time_last_cam_change: Instant,
 }
