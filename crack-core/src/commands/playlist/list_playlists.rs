9295dd3c1637c518510838b124797179377c2912
Cycle Five <cycle.five@proton.me>
2024-03-21 14:32:12 +0000

    Mar 21, 2024, 9:32 AM

---
+++ crack-core/src/commands/playlist/list_playlists.rs
@@ -0,0 +1,42 @@
+use crate::{db::playlist::Playlist, Context, Error};
+
+/// Get a playlist
+#[cfg(not(tarpaulin_include))]
+#[poise::command(prefix_command, slash_command, rename = "list")]
+pub async fn list_playlists(ctx: Context<'_>) -> Result<(), Error> {
+    let user_id = ctx.author().id.get() as i64;
+    let pool = ctx.data().database_pool.as_ref().unwrap();
+    let playlists = get_playlists_by_user_id(user_id, pool).await;
+
+    let embed = crate::utils::build_tracks_embed_metadata(&aux_metadata, 1).await;
+
+    // Send the embed
+    crate::utils::send_embed_response_poise(ctx, embed).await?;
+
+    Ok(())
+}
+
+/// Get the playlists for a given user_id.
+pub async fn get_playlists_by_user_id(user_id: UserId, pool: Pool) -> Vec<AuxMetadata> {
+    let metadata: Vec<crate::db::Metadata> = match playlist.parse::<i32>() {
+        // Try to parse the playlist as an ID
+        Ok(playlist_id) => Playlist::get_track_metadata_for_playlist(pool, playlist_id).await?,
+        Err(_) => {
+            let user_id = ctx.author().id.get() as i64;
+
+            Playlist::get_track_metadata_for_playlist_name(pool, playlist, user_id).await?
+        }
+    };
+    // Assuming you have a way to fetch the user_id of the command issuer
+    let aux_metadata = metadata
+        .iter()
+        .flat_map(|m| match crate::db::metadata::aux_metadata_from_db(m) {
+            Ok(aux) => Some(aux),
+            Err(e) => {
+                tracing::error!("Error converting metadata to aux metadata: {}", e);
+                None
+            }
+        })
+        .collect::<Vec<_>>();
+    // playlist.print_playlist(ctx).await?;
+}
