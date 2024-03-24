79e91e0cf6d4b4f39e7e7adba19bc301f489c24d
Cycle Five <cycle.five@proton.me>
2024-03-24 16:32:12 +0000

    Mar 24, 2024, 11:32 AM

--- crack-core/src/db/play_log.rs
+++ crack-core/src/db/play_log.rs
@@ -23,11 +23,11 @@ struct TitleArtist {
 
 impl Display for TitleArtist {
     fn fmt(&self, fmt: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
-        let title = self.title.unwrap_or_default();
-        let _ = fmt.write_str(&title);
+        let title = self.title.as_deref().unwrap_or_default();
+        let _ = fmt.write_str(title);
         let _ = fmt.write_str(" - ");
-        let artist = self.artist.unwrap_or_default();
-        let _ = fmt.write_str(&artist);
+        let artist = self.artist.as_deref().unwrap_or_default();
+        let _ = fmt.write_str(artist);
         Ok(())
     }
 }
