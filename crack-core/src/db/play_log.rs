c0017d74f6ee0f10009e14155f8aa97f5fac47f0
Cycle Five <cycle.five@proton.me>
2024-03-24 16:35:06 +0000

    Mar 24, 2024, 11:35 AM

--- crack-core/src/db/play_log.rs
+++ crack-core/src/db/play_log.rs
@@ -33,6 +33,7 @@ impl Display for TitleArtist {
 }
 
 impl PlayLog {
+    /// Create a new play log entry.
     pub async fn create(
         conn: &PgPool,
         user_id: i64,
@@ -66,7 +67,6 @@ impl PlayLog {
         } else if user_id.is_none() && guild_id.is_some() {
             Self::get_last_played_by_guild(conn, guild_id.unwrap()).await
         } else {
-            // user_id.is_some() && guild_id.is_none()
             Self::get_last_played_by_user(conn, user_id.unwrap()).await
         }
     }
@@ -101,6 +101,7 @@ impl PlayLog {
         Ok(last_played.into_iter().map(|t| t.to_string()).collect())
     }
 
+    /// Get the last played track for the given guild.
     pub async fn get_last_played_by_guild(
         conn: &PgPool,
         guild_id: i64,
@@ -108,6 +109,7 @@ impl PlayLog {
         Self::get_last_played_by_guild_filter(conn, guild_id, 1).await
     }
 
+    /// Get the last played track for the given guild and return as metadata.
     pub async fn get_last_played_by_guild_metadata(
         conn: &PgPool,
         guild_id: i64,
@@ -132,6 +134,7 @@ impl PlayLog {
         Ok(last_played.into_iter().map(|t| t.id as i64).collect())
     }
 
+    /// Get the last played track for the given user.
     pub async fn get_last_played_by_user(
         conn: &PgPool,
         user_id: i64,
