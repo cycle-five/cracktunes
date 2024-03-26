acc0e3da4c751158913242b7826965cf12b5ac62
Cycle Five <cycle.five@proton.me>
2024-03-26 17:14:54 +0000

    page 0 is first

--- crack-core/src/commands/playlist/list_playlists.rs
+++ crack-core/src/commands/playlist/list_playlists.rs
@@ -14,7 +14,7 @@ pub async fn list_playlists(ctx: Context<'_>) -> Result<(), Error> {
         .ok_or(CrackedError::NoDatabasePool)?;
     let playlists = Playlist::get_playlists_by_user_id(pool, user_id).await?;
 
-    let embed = build_playlist_list_embed(&playlists, 1).await;
+    let embed = build_playlist_list_embed(&playlists, 0).await;
 
     // Send the embed
     send_embed_response_poise(ctx, embed).await?;
