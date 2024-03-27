353c0a417395e8592ace54b471a8198949a04f35
Cycle Five <cycle.five@proton.me>
2024-03-27 08:28:00 +0000

    zxcv:

--- crack-core/src/commands/playlist/play_playlist.rs
+++ crack-core/src/commands/playlist/play_playlist.rs
@@ -1,4 +1,5 @@
 use super::get_playlist::get_playlist_;
+use crate::commands::{queue_aux_metadata, send_response_poise, CrackedMessage};
 use crate::{Context, Error};
 
 /// Get a playlist
@@ -10,23 +11,12 @@ pub async fn play_playlist(
     #[description = "Playlist name or id"]
     playlist: String,
 ) -> Result<(), Error> {
-    use crate::commands::{queue_aux_metadata, send_response_poise, CrackedMessage};
-
     let (aux_metadata, playlist_name) = get_playlist_(ctx, playlist).await?;
-    // let user_id = ctx.author().id.get() as i64;
-    // let pool = ctx
-    //     .data()
-    //     .database_pool
-    //     .as_ref()
-    //     .ok_or(CrackedError::NoDatabasePool)?;
-    // let playlists = Playlist::get_playlists_by_user_id(pool, user_id).await?;
+
     send_response_poise(ctx, CrackedMessage::PlaylistQueuing(playlist_name), true).await?;
-    let _ = queue_aux_metadata(ctx, aux_metadata).await?;
 
-    // let embed = build_playlist_list_embed(&playlists, 0).await;
+    queue_aux_metadata(ctx, aux_metadata).await?;
 
-    // // Send the embed
-    // send_embed_response_poise(ctx, embed).await?;
     send_response_poise(ctx, CrackedMessage::PlaylistQueued, true).await?;
 
     Ok(())
