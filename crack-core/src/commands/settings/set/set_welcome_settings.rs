ab6d9272a7f516aaf77c7492b1fc3351234a7e0d
Cycle Five <cycle.five@proton.me>
2024-03-27 14:48:15 +0000

    asdf

--- crack-core/src/commands/settings/set/set_welcome_settings.rs
+++ crack-core/src/commands/settings/set/set_welcome_settings.rs
@@ -23,36 +23,44 @@ pub async fn welcome_settings(
         message: Some(message.clone()),
         auto_role: None,
     };
-    let msg = {
-        let res = {
-            let mut write_guard = ctx.data().guild_settings_map.write().unwrap();
-            write_guard
-                .entry(guild_id)
-                .and_modify(|e| {
-                    e.set_welcome_settings3(channel.id().get(), message.clone());
-                })
-                .or_insert_with(|| {
-                    // GuildEntity::new_guild()
-                    GuildSettings::new(
-                        guild_id,
-                        Some(&prefix),
-                        get_guild_name(ctx.serenity_context(), guild_id),
-                    )
-                    .with_welcome_settings(Some(welcome_settings))
-                })
-                .welcome_settings
-                .clone()
-        };
-        match res {
-            Some(welcome_settings) => {
-                welcome_settings
-                    .save(&ctx.data().database_pool.clone().unwrap(), guild_id.get())
-                    .await?;
-                welcome_settings.to_string()
-            }
-            None => "Welcome settings failed to update?!?".to_string(),
-        }
-    };
+    // let msg = {
+    //     let res = {
+    //         let mut write_guard = ctx.data().guild_settings_map.write().unwrap();
+    //         write_guard
+    //             .entry(guild_id)
+    //             .and_modify(|e| {
+    //                 e.set_welcome_settings3(channel.id().get(), message.clone());
+    //             })
+    //             .or_insert_with(|| {
+    //                 // GuildEntity::new_guild()
+    //                 GuildSettings::new(
+    //                     guild_id,
+    //                     Some(&prefix),
+    //                     get_guild_name(ctx.serenity_context(), guild_id),
+    //                 )
+    //                 .with_welcome_settings(Some(welcome_settings))
+    //             })
+    //             .welcome_settings
+    //             .clone()
+    //     };
+    //     match res {
+    //         Some(welcome_settings) => {
+    //             welcome_settings
+    //                 .save(&ctx.data().database_pool.clone().unwrap(), guild_id.get())
+    //                 .await?;
+    //             welcome_settings.to_string()
+    //         }
+    //         None => "Welcome settings failed to update?!?".to_string(),
+    //     }
+    // };
+    let msg = set_welcome_settings(
+        ctx.data().clone(),
+        guild_id,
+        get_guild_name(ctx.serenity_context(), guild_id),
+        prefix.to_string(),
+        welcome_settings,
+    )
+    .await?;
     ctx.say(msg).await?;
     Ok(())
 }
@@ -99,6 +107,7 @@ mod test {
     use super::set_welcome_settings;
     use crate::guild::settings::{GuildSettingsMapParam, WelcomeSettings};
     use serenity::model::id::GuildId;
+    use tokio;
 
     #[tokio::test]
     async fn test_set_welcome_settings() {
