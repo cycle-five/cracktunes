e5d440aab59fd7ddf2706172fb74b5bfb544cedc
Cycle Five <cycle.five@proton.me>
2024-03-30 05:56:43 +0000

    Mar 30, 2024, 12:56 AM

---
+++ crack-core/src/commands/playlist/load_playlist.rs
@@ -0,0 +1,56 @@
+use songbird::input::AuxMetadata;
+
+use crate::{
+    db::{metadata::aux_metadata_from_db, playlist::Playlist, Metadata},
+    utils::{build_tracks_embed_metadata, send_embed_response_poise},
+    Context, CrackedError, Error,
+};
+
+/// Get a playlist
+#[cfg(not(tarpaulin_include))]
+#[poise::command(prefix_command, slash_command, rename = "get")]
+pub async fn get_playlist(ctx: Context<'_>, #[rest] playlist: String) -> Result<(), Error> {
+    let (aux_metadata, playlist_name): (Vec<AuxMetadata>, String) =
+        get_playlist_(ctx, playlist).await?;
+    let embed = build_tracks_embed_metadata(playlist_name, &aux_metadata, 0).await;
+
+    // Send the embed
+    send_embed_response_poise(ctx, embed).await?;
+
+    Ok(())
+}
+
+/// Get a playlist by name or id
+pub async fn get_playlist_(
+    ctx: Context<'_>,
+    playlist: String,
+) -> Result<(Vec<AuxMetadata>, String), Error> {
+    let pool = ctx
+        .data()
+        .database_pool
+        .as_ref()
+        .ok_or(CrackedError::NoDatabasePool)?;
+
+    let metadata: Vec<Metadata> = match playlist.parse::<i32>() {
+        // Try to parse the playlist as an ID
+        Ok(playlist_id) => Playlist::get_track_metadata_for_playlist(pool, playlist_id).await?,
+        Err(_) => {
+            let user_id = ctx.author().id.get() as i64;
+
+            Playlist::get_track_metadata_for_playlist_name(pool, playlist.clone(), user_id).await?
+        }
+    };
+    // Assuming you have a way to fetch the user_id of the command issuer
+    let aux_metadata = metadata
+        .iter()
+        .flat_map(|m| match aux_metadata_from_db(m) {
+            Ok(aux) => Some(aux),
+            Err(e) => {
+                tracing::error!("Error converting metadata to aux metadata: {}", e);
+                None
+            }
+        })
+        .collect::<Vec<_>>();
+    // playlist.print_playlist(ctx).await?;
+    Ok((aux_metadata, playlist))
+}
