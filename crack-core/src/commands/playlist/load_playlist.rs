5d8ae2d185f2a34272528f745ba644dabddd80b7
Cycle Five <cycle.five@proton.me>
2024-03-30 17:52:42 +0000

    Mar 30, 2024, 12:52 PM

--- crack-core/src/commands/playlist/load_playlist.rs
+++ crack-core/src/commands/playlist/load_playlist.rs
@@ -7,19 +7,19 @@ use crate::{
     utils::send_embed_response_str,
     Context, CrackedError, Error,
 };
-use songbird::input::Input as SongbirdInput;
+use songbird::input::{AuxMetadata, Input as SongbirdInput};
 
 use url::Url;
 
-#[macro_export]
-macro_rules! get_query_type_or_err {
-    ($ctx:expr, $url:expr) => {
-        match get_query_type_from_url($ctx, $url.as_ref(), None).await? {
-            Some(QueryType::KeywordList(v)) => QueryType::KeywordList(v),
-            _ => return Err(CrackedError::Other("Bad Query Type").into()),
-        }
-    };
-}
+// #[macro_export]
+// macro_rules! get_query_type_or_err {
+//     ($ctx:expr, $url:expr) => {
+//         match get_query_type_from_url($ctx, $url.as_ref(), None).await? {
+//             Some(QueryType::KeywordList(v)) => QueryType::KeywordList(v),
+//             _ => return Err(CrackedError::Other("Bad Query Type").into()),
+//         }
+//     };
+// }
 
 #[macro_export]
 macro_rules! get_db_or_err {
@@ -31,42 +31,27 @@ macro_rules! get_db_or_err {
     };
 }
 
-/// Get a playlist
-#[cfg(not(tarpaulin_include))]
-#[poise::command(prefix_command, slash_command, rename = "loadspotify")]
-pub async fn loadspotify(
+pub async fn loadspotify_(
     ctx: Context<'_>,
-    #[description = "Name of the playlist to create and load into."] name: String,
-    #[rest]
-    #[description = "Spotify.com url to the *public* playlist."]
+    name: String,
     spotifyurl: String,
-) -> Result<(), Error> {
-    // verify url format
+) -> Result<(Vec<AuxMetadata>, SongbirdInput), Error> {
     let guild_id = ctx.guild_id().ok_or(CrackedError::NoGuildId)?;
     let channel_id = ctx.channel_id();
 
     let url_clean = Url::parse(&spotifyurl.clone())?;
+    let query_type: QueryType = match get_query_type_from_url(ctx, url_clean.as_ref(), None).await?
+    {
+        Some(QueryType::KeywordList(v)) => QueryType::KeywordList(v),
+        _ => return Err(CrackedError::Other("Bad Query Type").into()),
+    };
 
-    let query_type: QueryType = get_query_type_or_err!(ctx, url_clean);
-    // let query_type: QueryType = match get_query_type_from_url(ctx, url_clean.as_ref(), None).await?
-    // {
-    //     Some(QueryType::KeywordList(v)) => QueryType::KeywordList(v),
-    //     _ => return Err(CrackedError::Other("Bad Query Type").into()),
-    // };
-
-    // let (aux_metadata, playlist_name): (Vec<AuxMetadata>, String) =
-    //     get_playlist_(ctx, playlist).await?;
     let (_source, metadata): (SongbirdInput, Vec<MyAuxMetadata>) =
         get_track_source_and_metadata(ctx.http(), query_type.clone()).await;
-    // // let embed = build_tracks_embed_metadata(playlist_name, &aux_metadata, 0).await;
-    // let db_pool = ctx
-    //     .data()
-    //     .database_pool
-    //     .as_ref()
-    //     .ok_or(CrackedError::NoDatabasePool)?;
 
     let db_pool = get_db_or_err!(ctx);
 
+    let metadata_vec: Vec<AuxMetadata> = Vec::new();
     let playls = Playlist::create(db_pool, &name.clone(), ctx.author().id.get() as i64).await?;
     let guild_id_i64 = guild_id.get() as i64;
     let channel_id_i64 = channel_id.get() as i64;
@@ -90,30 +75,25 @@ pub async fn loadspotify(
             }
         }
     }
+    Ok((metadata_vec, _source))
+}
+
+/// Get a playlist
+#[cfg(not(tarpaulin_include))]
+#[poise::command(prefix_command, slash_command)]
+pub async fn loadspotify(
+    ctx: Context<'_>,
+    #[description = "Name of the playlist to create and load into."] name: String,
+    #[rest]
+    #[description = "Spotify.com url to the *public* playlist."]
+    spotifyurl: String,
+) -> Result<(), Error> {
+    // // verify url format
+
+    let (_metadata_vec, _source) = loadspotify_(ctx, name.to_string(), spotifyurl).await?;
 
     // Send the embed
     send_embed_response_str(ctx, CrackedMessage::PlaylistCreated(name).to_string()).await?;
 
     Ok(())
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-    use crate::db::aux_metadata_to_db_structures;
-    use crate::db::playlist::Playlist;
-    use crate::db::playlist::PlaylistTrack;
-    use crate::db::Metadata;
-
-    #[test]
-    fn test_get_query_type_or_err() {
-        let ctx = Context::mock();
-        let url = "https://open.spotify.com/playlist/37i9dQZF1DXcBWIGoYBM5M?si=8b1b1b1b1b1b1b1b";
-        let res = get_query_type_or_err!(ctx, url);
-        assert!(matches!(res, QueryType::KeywordList(_)));
-        assert_eq!(
-            res,
-            QueryType::KeywordList(vec!["37i9dQZF1DXcBWIGoYBM5M".to_string()])
-        );
-    }
 }
