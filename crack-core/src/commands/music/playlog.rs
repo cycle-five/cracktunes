6f8195801a60788378c57d3c67c11ab798ba5ebf
Cycle Five <cycle.five@proton.me>
2024-03-30 01:12:27 +0000

    Mar 29, 2024, 8:12 PM

--- crack-core/src/commands/music/playlog.rs
+++ crack-core/src/commands/music/playlog.rs
@@ -21,3 +21,23 @@ pub async fn playlog(ctx: Context<'_>) -> Result<(), Error> {
 
     Ok(())
 }
+
+/// Get recently played tracks.
+#[cfg(not(tarpaulin_include))]
+#[poise::command(slash_command, prefix_command, guild_only)]
+pub async fn myplaylog(ctx: Context<'_>) -> Result<(), Error> {
+    let guild_id = ctx.guild_id().unwrap();
+
+    let last_played = PlayLog::get_last_played(
+        ctx.data().database_pool.as_ref().unwrap(),
+        None,
+        Some(guild_id.get() as i64),
+    )
+    .await?;
+
+    let msg = send_response_poise(ctx, CrackedMessage::PlayLog(last_played), true).await?;
+    ctx.data().add_msg_to_cache(guild_id, msg);
+
+    Ok(())
+}
+
