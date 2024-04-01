4d417d90a738ee813a3840dbb0b6fcccf4ba5170
Cycle Five <cycle.five@proton.me>
2024-04-01 00:12:09 +0000

    Mar 31, 2024, 7:12 PM

--- crack-core/src/commands/music/playlog.rs
+++ crack-core/src/commands/music/playlog.rs
@@ -3,13 +3,15 @@ use crate::messaging::message::CrackedMessage;
 use crate::utils::send_response_poise;
 use crate::{Context, Error};
 
-/// Get recently played tracks.
+/// Get recently played tracks form the guild.
 #[cfg(not(tarpaulin_include))]
 #[poise::command(slash_command, prefix_command, guild_only)]
 pub async fn playlog(ctx: Context<'_>) -> Result<(), Error> {
     playlog_(ctx).await
 }
 
+/// Get recently played tracks for the guild.
+#[cfg(not(tarpaulin_include))]
 pub async fn playlog_(ctx: Context<'_>) -> Result<(), Error> {
     let guild_id = ctx.guild_id().unwrap();
 
@@ -26,10 +28,15 @@ pub async fn playlog_(ctx: Context<'_>) -> Result<(), Error> {
     Ok(())
 }
 
-/// Get recently played tracks.
+/// Get recently played tracks for the calling user in the guild.
 #[cfg(not(tarpaulin_include))]
 #[poise::command(slash_command, prefix_command, guild_only)]
 pub async fn myplaylog(ctx: Context<'_>) -> Result<(), Error> {
+    myplaylog_(ctx).await
+}
+
+#[cfg(not(tarpaulin_include))]
+pub async fn myplaylog_(ctx: Context<'_>) -> Result<(), Error> {
     let guild_id = ctx.guild_id().unwrap();
     let user_id = ctx.author().id;
 
