d4fcd86e887aba5a7361eca8e210828c801ee04a
Cycle Five <cycle.five@proton.me>
2024-04-12 11:44:12 +0000

    Apr 12, 2024, 6:44 AM

--- crack-core/src/commands/settings/toggle/self_deafen.rs
+++ crack-core/src/commands/settings/toggle/self_deafen.rs
@@ -1,16 +1,25 @@
-use tracing::instrument;
-
 use crate::{
-    errors::CrackedError, guild::settings::GuildSettings, utils::get_guild_name, Context, Error,
+    errors::CrackedError, guild::settings::GuildSettings, utils::get_guild_name, Context, Data,
+    Error,
 };
+use serenity::all::GuildId;
+use sqlx::PgPool;
 
 /// Toggle the self deafen for the bot.
 #[poise::command(prefix_command, owners_only, ephemeral)]
 #[cfg(not(tarpaulin_include))]
 pub async fn self_deafen(ctx: Context<'_>) -> Result<(), Error> {
-    use crate::utils::{send_embed_response, send_embed_response_poise};
-
-    let res = self_deafen_(ctx).await?;
+    let res = toggle_self_deafen(
+        ctx.data().clone(),
+        ctx.data()
+            .database_pool
+            .clone()
+            .ok_or(CrackedError::NoDatabasePool)?,
+        ctx.guild_id().ok_or(CrackedError::NoGuildId)?,
+        get_guild_name(ctx.serenity_context(), ctx.guild_id().unwrap()),
+        ctx.data().bot_settings.get_prefix(),
+    )
+    .await?;
 
     ctx.say(format!("Self-deafen is now {}", res.self_deafen))
         .await?;
@@ -19,28 +28,27 @@ pub async fn self_deafen(ctx: Context<'_>) -> Result<(), Error> {
 
 /// Toggle the self deafen for the bot.
 #[cfg(not(tarpaulin_include))]
-pub async fn self_deafen_(ctx: Context<'_>) -> Result<GuildSettings, Error> {
-    let guild_id = ctx.guild_id().ok_or(CrackedError::NoGuildId)?;
-
-    let res = ctx
-        .data()
+pub async fn toggle_self_deafen(
+    data: Data,
+    pool: PgPool,
+    guild_id: GuildId,
+    guild_name: Option<String>,
+    prefix: String,
+) -> Result<GuildSettings, CrackedError> {
+    let res = data
         .guild_settings_map
         .write()
         .unwrap()
-        .entry(ctx.guild_id().unwrap())
+        .entry(guild_id)
         .and_modify(|e| {
             e.toggle_self_deafen();
         })
         .or_insert_with(|| {
-            GuildSettings::new(
-                ctx.guild_id().unwrap(),
-                Some(&ctx.data().bot_settings.get_prefix()),
-                get_guild_name(ctx.serenity_context(), guild_id),
-            )
-            .toggle_self_deafen()
-            .clone()
+            GuildSettings::new(guild_id, Some(&prefix), guild_name)
+                .toggle_self_deafen()
+                .clone()
         })
         .clone();
-    let _ = res.save(&ctx.data().database_pool.clone().unwrap()).await?;
+    let _ = res.save(&pool).await?;
     Ok(res)
 }
