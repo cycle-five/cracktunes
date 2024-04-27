023b99c2b5911e6fadf98527ef3b0ee8293bf3b3
Cycle Five <cycle.five@proton.me>
2024-04-27 20:08:46 +0000

    add NoChannelId error

--- crack-core/src/errors.rs
+++ crack-core/src/errors.rs
@@ -2,9 +2,9 @@ use crate::messaging::messages::{
     EMPTY_SEARCH_RESULT, FAIL_ANOTHER_CHANNEL, FAIL_AUTHOR_DISCONNECTED, FAIL_AUTHOR_NOT_FOUND,
     FAIL_EMPTY_VECTOR, FAIL_INVALID_TOPGG_TOKEN, FAIL_NOTHING_PLAYING, FAIL_NOT_IMPLEMENTED,
     FAIL_NO_SONGBIRD, FAIL_NO_VIRUSTOTAL_API_KEY, FAIL_NO_VOICE_CONNECTION, FAIL_PARSE_TIME,
-    FAIL_PLAYLIST_FETCH, FAIL_WRONG_CHANNEL, GUILD_ONLY, NO_DATABASE_POOL, NO_GUILD_CACHED,
-    NO_GUILD_ID, NO_GUILD_SETTINGS, QUEUE_IS_EMPTY, ROLE_NOT_FOUND, SPOTIFY_AUTH_FAILED,
-    UNAUTHORIZED_USER,
+    FAIL_PLAYLIST_FETCH, FAIL_WRONG_CHANNEL, GUILD_ONLY, NO_CHANNEL_ID, NO_DATABASE_POOL,
+    NO_GUILD_CACHED, NO_GUILD_ID, NO_GUILD_SETTINGS, QUEUE_IS_EMPTY, ROLE_NOT_FOUND,
+    SPOTIFY_AUTH_FAILED, UNAUTHORIZED_USER,
 };
 use crate::Error;
 use audiopus::error::Error as AudiopusError;
@@ -42,6 +42,7 @@ pub enum CrackedError {
     LogChannelWarning(&'static str, GuildId),
     NotInRange(&'static str, isize, isize, isize),
     NotConnected,
+    NoChannelId,
     NotImplemented,
     NoTrackName,
     NoDatabasePool,
@@ -125,6 +126,7 @@ impl Display for CrackedError {
             Self::NotInRange(param, value, lower, upper) => f.write_str(&format!(
                 "`{param}` should be between {lower} and {upper} but was {value}"
             )),
+            Self::NoChannelId => f.write_str(NO_CHANNEL_ID),
             Self::NotConnected => f.write_str(FAIL_NO_VOICE_CONNECTION),
             Self::NotImplemented => f.write_str(FAIL_NOT_IMPLEMENTED),
             Self::NoTrackName => f.write_str("No track name"),
