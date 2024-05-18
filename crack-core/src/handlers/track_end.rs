24c3ca23c11656caa11ab34809c2d5b939a568ae
Cycle Five <cycle.five@proton.me>
2024-05-18 06:10:04 +0000

    fmt

--- crack-core/src/handlers/track_end.rs
+++ crack-core/src/handlers/track_end.rs
@@ -15,7 +15,10 @@ use crate::{
     db::PlayLog,
     errors::{verify, CrackedError},
     guild::operations::GuildSettingsOperations,
-    messaging::{interface::{build_nav_btns, create_queue_embed}, messages::SPOTIFY_AUTH_FAILED},
+    messaging::{
+        interface::{build_nav_btns, create_queue_embed},
+        messages::SPOTIFY_AUTH_FAILED,
+    },
     sources::spotify::{Spotify, SPOTIFY},
     utils::{calculate_num_pages, forget_queue_message, send_now_playing},
     Data, Error,
