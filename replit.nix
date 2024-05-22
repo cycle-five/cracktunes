cd3af25e8e7f502467770976732abcbd9a5601ec
Cycle Five <107008871+cycle-five@users.noreply.github.com>
2024-05-22 18:10:36 +0000

    Config replit ignores

--- replit.nix
+++ replit.nix
@@ -3,13 +3,17 @@
   nixpkgs.overlays = [
     (import "${fetchTarball "https://github.com/nix-community/fenix/archive/main.tar.gz"}/overlay.nix")
   ];
-  environment.systemPackages = with pkgs; [
+  
+  deps = [
+    pkgs.cmake
+  ];environment.systemPackages = with pkgs; [
     (fenix.complete.withComponents [
       "cargo"
       "clippy"
       "rust-src"
       "rustc"
       "rustfmt"
+      "cmake"
     ])
     rust-analyzer-nightly
   ];
