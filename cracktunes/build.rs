0a060e4b8863145604cceee5f4937d66a7bf5543
Cycle Five <cycle.five@proton.me>
2024-08-15 19:06:26 +0000

    update builds

--- cracktunes/build.rs
+++ cracktunes/build.rs
@@ -1,23 +1,15 @@
 use vergen_gitcl as vergen;
-// use std::process::Command;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // make sure tarpaulin is included in the build.
     println!("cargo:rustc-check-cfg=cfg(tarpaulin_include)");
     // NOTE: This will output everything, and requires all features enabled.
     // NOTE: See the specific builder documentation for configuration options.
     let build = vergen::BuildBuilder::all_build()?;
-    // let cargo = vergen::CargoBuilder::all_cargo()?;
     let git = vergen::GitclBuilder::all_git()?;
-    // let rustc = vergen::RustcBuilder::all_rustc()?;
-    // let si = vergen::SysinfoBuilder::all_sysinfo()?;
 
     vergen::Emitter::default()
         .add_instructions(&build)?
         .add_instructions(&git)?
-        // .add_instructions(&cargo)?
-        // .add_instructions(&git2)?
-        // .add_instructions(&rustc)?
-        // .add_instructions(&si)?
         .emit()?;
     Ok(())
 }
