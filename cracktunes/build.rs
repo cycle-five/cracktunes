88c062359912e285b59ba3279b692f149bc9b415
Cycle Five <cycle.five@proton.me>
2024-08-15 16:29:49 +0000

    Trim vergen dep

--- cracktunes/build.rs
+++ cracktunes/build.rs
@@ -1,4 +1,4 @@
-use vergen_git2 as vergen;
+use vergen_gitcl as vergen;
 // use std::process::Command;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // make sure tarpaulin is included in the build.
@@ -6,17 +6,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // NOTE: This will output everything, and requires all features enabled.
     // NOTE: See the specific builder documentation for configuration options.
     let build = vergen::BuildBuilder::all_build()?;
-    let cargo = vergen::CargoBuilder::all_cargo()?;
-    let git2 = vergen::Git2Builder::all_git()?;
-    let rustc = vergen::RustcBuilder::all_rustc()?;
-    let si = vergen::SysinfoBuilder::all_sysinfo()?;
+    // let cargo = vergen::CargoBuilder::all_cargo()?;
+    let git = vergen::GitclBuilder::all_git()?;
+    // let rustc = vergen::RustcBuilder::all_rustc()?;
+    // let si = vergen::SysinfoBuilder::all_sysinfo()?;
 
     vergen::Emitter::default()
         .add_instructions(&build)?
-        .add_instructions(&cargo)?
-        .add_instructions(&git2)?
-        .add_instructions(&rustc)?
-        .add_instructions(&si)?
+        .add_instructions(&git)?
+        // .add_instructions(&cargo)?
+        // .add_instructions(&git2)?
+        // .add_instructions(&rustc)?
+        // .add_instructions(&si)?
         .emit()?;
     Ok(())
 }
