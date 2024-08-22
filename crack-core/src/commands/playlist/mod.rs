280378e14c06da644d042f14a32ae13aab72ae22
Cycle Five <cycle.five@proton.me>
2024-08-22 16:56:52 +0000

    asdf

--- crack-core/src/commands/playlist/mod.rs
+++ crack-core/src/commands/playlist/mod.rs
@@ -51,15 +51,13 @@ pub async fn playlist(ctx: Context<'_>) -> Result<(), Error> {
     Ok(())
 }
 
-pub fn commands() -> [crate::Command; 8] {
-    [
-        playlist(),
-        // addto(),
-        // create(),
-        // delete(),
-        // get(),
-        // list(),
-        // pplay(),
-        // loadspotify(),
-    ]
+pub fn commands() -> [crate::Command; 1] {
+    [playlist()]
+    // addto(),
+    // create(),
+    // delete(),
+    // get(),
+    // list(),
+    // pplay(),
+    // loadspotify(),
 }
