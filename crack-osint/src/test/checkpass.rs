117aa637dc7d9b1e4816c203b21dd68d45df2e94
Cycle Five <cycle.five@proton.me>
2024-10-12 22:55:54 +0000

    crack-osint

--- crack-osint/src/test/checkpass.rs
+++ crack-osint/src/test/checkpass.rs
@@ -1,11 +1,13 @@
 #[cfg(test)]
 mod test {
     use crate::check_password_pwned;
+    use reqwest::Client;
 
     #[tokio::test]
     async fn test_check_password_pwned() {
+        let client = Client::new();
         // Test the check_password_pwned function with a known pwned password
-        let result = check_password_pwned("password123").await;
+        let result = check_password_pwned(&client, "password123").await;
 
         // Assert that the result is Ok and true
         assert_eq!(result.unwrap(), true);
